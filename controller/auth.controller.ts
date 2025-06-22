f91cd927d93d110149afb3da3373b1de4821d466
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-06-23 03:53:11 +0530

    added auth checks
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

--- controller/auth.controller.ts
+++ controller/auth.controller.ts
@@ -4,6 +4,10 @@ import jwt from 'jsonwebtoken';
 export const getAdminToken = (req: Request, res: Response) => {
   const { username, password } = req.body;
 
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
   if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
     const token = jwt.sign({ username }, process.env.JWT_SECRET!, { expiresIn: '1h' });
     return res.status(200).json({ token });
